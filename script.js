c16150b2f436f533780c09ae073c30f99cbd9a8c
timble <timblemichael20@gmail.com>
2022-04-01 11:27:12 +1000

    nav bar functionalities

--- script.js
+++ script.js
@@ -1,13 +1,76 @@
-let button = document.getElementById('button')
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-function grab() {
-    fetch('https://swapi.dev/api/people/1')
+tabs.forEach(tab => {
+    tab.addEventListener('click',() => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+
+
+let peopleButton = document.getElementById('people-button')
+let planetButton = document.getElementById('planet-button')
+
+
+function getPeople() {
+
+    let numberPeople = Math.floor((Math.random()*82)+1)
+    let apiUrl = 'https://swapi.dev/api/people/' +  numberPeople 
+
+    fetch(apiUrl)
     .then(function(response){
         return response.json()
     })
     .then (function(json){
         console.log(json)
+        let name = document.getElementById('name')
+        let DOB = document.getElementById('DOB')
+        let height = document.getElementById('height')
+        let weight = document.getElementById('weight')
+        let gender = document.getElementById('gender')
+
+        name.innerText = `Name: ${json['name']}`;
+        DOB.innerText = `Date of Brith: ${json['birth_year']}`;
+        height.innerText = `Height: ${json['height']}`;
+        weight.innerText = `Weight: ${json['mass']}`;
+        gender.innerText = `Gender: ${json['gender']}`;
     })
 }
 
-button.addEventListener('click',grab)
+function getPlanet() {
+
+    let numberPlanet = Math.floor((Math.random()*60)+1)
+    let apiUrl = 'https://swapi.dev/api/planets/' +  numberPlanet
+
+    fetch(apiUrl)
+    .then(function(response){
+        return response.json()
+    })
+    .then (function(json){
+        console.log(json)
+        let name = document.getElementById('planet-name')
+        let climate = document.getElementById('climate')
+        let terrain = document.getElementById('terrain')
+        let population = document.getElementById('population')
+        let rotationPeriod = document.getElementById('rotation-period')
+
+        name.innerText = `Name: ${json['name']}`;
+        climate.innerText = `Climate: ${json['climate']}`;
+        terrain.innerText = `Terrain: ${json['terrain']}`;
+        population.innerText = `Population: ${json['population']}`;
+        rotationPeriod.innerText = `Rotation Period: ${json['rotation_period']}`;
+
+    })
+}
+
+
+peopleButton.addEventListener('click', getPeople)
+planetButton.addEventListener('click', getPlanet)
